a7878ef747ed61cec6ee67eaa046de624d2a01ee
Hassanamiri <hassanamiri369@gmail.com>
2022-03-14 18:14:58 +0330

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+   <>
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+
+      <main>List products</main>
+    </div>
+   </>
+  );
+}
+
+export default App;
