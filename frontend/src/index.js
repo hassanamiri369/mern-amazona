985ea2f00ee93307ef8f262e1a33bcc41924460b
Hassanamiri <hassanamiri369@gmail.com>
2022-03-16 02:28:55 +0330

    create context file and provide to index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,16 @@ import {HelmetProvider} from "react-helmet-async"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
+import { StoreProvider } from './Store';
 
 
 ReactDOM.render(
+  <StoreProvider>
+
   <HelmetProvider>
     <App />
   </HelmetProvider>
+  </StoreProvider>
     
  ,
   document.getElementById('root')
